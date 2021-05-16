0de1648de2a834c97db45b2af7a3a2500e39d6a3
SUMIT <sumitdogra3236@gmail.com>
2021-05-16 12:58:55 +0530

    Cherrio

--- Module_1/Lec_4_WebScrapping_Intro/facts/fs.js
+++ Module_1/Lec_4_WebScrapping_Intro/facts/fs.js
@@ -2,15 +2,18 @@ let fs = require("fs");
 
 let cheerio = require("cheerio");
 
-let htmlData = fs.readFileSync("./index1.html", "utf8");
+let htmlData = fs.readFileSync("./index.html", "utf8");
 
 let myDocument = cheerio.load(htmlData);
 let myDocumentText = cheerio.load(htmlData).text();
 
 let h1Data = myDocument("h1").text();
-let h2Data = myDocument("h2").text();
+// let h2Data = myDocument("h2").text();
 console.log(h1Data);
-console.log(h2Data);
+// console.log(h2Data);
 
+
+let ptagData = myDocument("p").text();
+console.log(ptagData);  
 
 

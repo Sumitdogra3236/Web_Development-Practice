db0715f93ea8bab064ff62262a446b2640f9a1a3
SUMIT <sumitdogra3236@gmail.com>
2021-05-16 13:12:24 +0530

    Cheerio

--- Module_1/Lec_4_WebScrapping_Intro/facts/fs.js
+++ Module_1/Lec_4_WebScrapping_Intro/facts/fs.js
@@ -9,11 +9,20 @@ let myDocumentText = cheerio.load(htmlData).text();
 
 let h1Data = myDocument("h1").text();
 // let h2Data = myDocument("h2").text();
-console.log(h1Data);
+// console.log(h1Data);
 // console.log(h2Data);
 
 
 let ptagData = myDocument("p").text();
-console.log(ptagData);  
+// console.log(ptagData);  
+//  let secondPTag = myDocument("p")[1];
+//  console.log(secondPTag.text()); --> doesn't work because text() function doesn't work in the nested object and only used with cheerio object 
+
+// console.log(secondPTag);
+
+// console.log(myDocument(secondPTag).text());
 
 
+
+//Selectors
+console.log(myDocument("ul p").text()); // it will give all the p tags inside ul

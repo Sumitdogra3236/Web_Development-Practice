ba6012a91e5c48f2d748f36c143a64767af43d5e
SUMIT <sumitdogra3236@gmail.com>
2021-05-16 10:43:16 +0530

    cheerio

---
+++ Module_1/Lec_4_WebScrapping_Intro/facts/fs.js
@@ -0,0 +1,16 @@
+let fs = require("fs");
+
+let cheerio = require("cheerio");
+
+let htmlData = fs.readFileSync("./index1.html", "utf8");
+
+let myDocument = cheerio.load(htmlData);
+let myDocumentText = cheerio.load(htmlData).text();
+
+let h1Data = myDocument("h1").text();
+let h2Data = myDocument("h2").text();
+console.log(h1Data);
+console.log(h2Data);
+
+
+

a00e14a3e5bfd6f7556c7a6f1ef4798750dc6ac4
SUMIT <sumitdogra3236@gmail.com>
2021-06-07 13:43:03 +0530

    Cheerio

--- Module_1/Lec_4_WebScrapping_Intro/activity/highestWicketTaker.js
+++ Module_1/Lec_4_WebScrapping_Intro/activity/highestWicketTaker.js
@@ -1,67 +1,65 @@
-let matchLink ="https://www.espncricinfo.com/series/ipl-2020-21-1210595/delhi-capitals-vs-mumbai-indians-final-1237181/full-scorecard";
-const request = require('request');
-const fs = require('fs');
-const cheerio = require('cheerio');
+let matchLink =
+  "https://www.espncricinfo.com/series/ipl-2020-21-1210595/delhi-capitals-vs-mumbai-indians-final-1237181/full-scorecard";
+const request = require("request");
+const fs = require("fs");
+const cheerio = require("cheerio");
 
-// request(matchLink, cb);
+request(matchLink, cb);
 
-// function cb(error , response , data){
-//     console.log("Got the Data");
-//     // console.log(error);
-//     // console.log(response);
-//     // console.log(data);
+function cb(error, response, data) {
+  fs.writeFileSync("./match.html", data);
+  getHighestWicketTaker(data);
+}
 
-//     fs.writeFileSync('./match.html', data);
-
-// }
-
-let htmlData = fs.readFileSync('./match.html', 'utf-8');
-
-let myDocument = cheerio.load(htmlData);
-// // console.log(myDocument);
-// let matchInfo = myDocument(".status-text span").text();
-// // console.log(matchInfo);
-
-let bothBowlingTables = myDocument(".table.bowler");
 // bothBowlingTables = {
-    // "0" : {bowling table},
-    // "1" : {bowling table}
+// "0" : {bowling table},
+// "1" : {bowling table}
 // }
-
-// // console.log(bothBowlingTables.length);
-// // console.log(bothBowlingTables.text());
 // fs.writeFileSync('./bowlingTables.html', bothBowlingTables+"");
 
-let highestWicketTakerName;
-let highestWicketTaken;
-let economyofHighestWicketTaker;
+function getHighestWicketTaker(data) {
+    let htmlData = fs.readFileSync("./match.html", "utf-8");
+    let myDocument = cheerio.load(htmlData);
+    let bothBowlingTables = myDocument(".table.bowler");
+    let highestWicketTakerName;
+    let highestWicketTaken;
+    
+    let economyofHighestWicketTaker;
 
-for(let i =0;i<bothBowlingTables.length;i++){
+  for (let i = 0; i < bothBowlingTables.length; i++) {
     let bowlingTable = myDocument(bothBowlingTables[i]);
     let allTableRows = bowlingTable.find(".table.bowler tbody tr ");
     // console.log(allTableRows.text());
 
-    for(let j = 0; j<allTableRows.length;j++){
-        let allTds = myDocument(allTableRows[j]).find("td");
-        if(i ==0 && j == 0){
-            highestWicketTakerName = myDocument(allTds[0]).find("a").text();
-            highestWicketTaken = myDocument(allTds[4]).text();
-            economyofHighestWicketTaker = myDocument(allTds[5]).text();
+    for (let j = 0; j < allTableRows.length; j++) {
+      let allTds = myDocument(allTableRows[j]).find("td");
+      if (i == 0 && j == 0) {
+        highestWicketTakerName = myDocument(allTds[0]).find("a").text();
+        highestWicketTaken = myDocument(allTds[4]).text();
+        economyofHighestWicketTaker = myDocument(allTds[5]).text();
+      } else {
+        let currentWickets = myDocument(allTds[4]).text();
+        let currentEconomy = myDocument(allTds[5]).text;
+        if (currentWickets > highestWicketTaken) {
+          highestWicketTakerName = myDocument(allTds[0]).find("a").text();
+          highestWicketTaken = myDocument(allTds[4]).text();
+          economyofHighestWicketTaker = myDocument(allTds[5]).text();
+        } else if (
+          currentWickets == highestWicketTaken &&
+          currentEconomy < economyofHighestWicketTaker
+        ) {
+          highestWicketTakerName = myDocument(allTds[0]).find("a").text();
+          highestWicketTaken = myDocument(allTds[4]).text();
+          economyofHighestWicketTaker = myDocument(allTds[5]).text();
         }
-        else {
-            let currentWickets = myDocument(allTds[4]).text();
-            if(currentWickets > highestWicketTaken){
-              highestWicketTakerName = myDocument(allTds[0]).find("a").text();
-              highestWicketTaken = myDocument(allTds[4]).text();
-              economyofHighestWicketTaker = myDocument(allTds[5]).text();  
-            }
-        }
+      }
     }
 
     // name : "0"
     // wicket : "4"
     // economy : "5"
+  }
+  console.log("Highest Wicket Taker Name : " + highestWicketTakerName);
+  console.log("Wicket Taken : " + highestWicketTaken);
+  console.log("Highest Wicket Taker Econmy" + economyofHighestWicketTaker);
 }
-console.log("Highest Wicket Taker Name : " +highestWicketTakerName);
-console.log("Wicket Taken : "+ highestWicketTaken);
-console.log("Highest Wicket Taker Econmy" + economyofHighestWicketTaker)

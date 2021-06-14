830a7b0f1a945f2fefb400b76e6611ff0dee0fbb
SUMIT <sumitdogra3236@gmail.com>
2021-06-14 13:07:01 +0530

    Async Theory

--- Module_1/Lec_6_Async/facts/async.js
+++ Module_1/Lec_6_Async/facts/async.js
@@ -1,3 +1,10 @@
+
+// GEC -> Global Execution Context -> where js code runs in the stack
+// Node API -> Where the callBack functions executes
+// Waiting Queue -> CallBack function are pushed to Waiting queue after their execution(i.e. fetching of the data from the files), for the further operation in GEC
+// Event Loop -> It checks whether the GEC is empty or not, if empty then only it sends the callBack functin for further execution from the Waiting Queue
+
+
 const fs = require("fs");
 
 console.log("start");

3e138b60add43aa86052a01cb8f3f49e563c6187
SUMIT <sumitdogra3236@gmail.com>
2021-06-14 12:43:21 +0530

    Async Parallel

--- Module_1/Lec_6_Async/facts/asyncParall.js
+++ Module_1/Lec_6_Async/facts/asyncParall.js
@@ -2,15 +2,15 @@ const fs = require("fs");
 
 console.log("Start");
 
-fs.readFile('./f1.txt', function cb(error , data){
+fs.readFile('./f1.txt', function (error , data){
     console.log(data+"");
 })
 
-fs.readFile('./f2.txt', function cb(error, data){
+fs.readFile('./f2.txt', function (error, data){
     console.log(data + "");
 })
 
-fs.readFile('./f3.txt', function cb(error , data){
+fs.readFile('./f3.txt', function (error , data){
     // console.log(err);
     console.log(data + "");
 })

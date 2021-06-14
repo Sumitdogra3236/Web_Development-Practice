be36a17a412db99440476966e8ebb06d287f0721
SUMIT <sumitdogra3236@gmail.com>
2021-06-14 12:43:32 +0530

    Async Serial

---
+++ Module_1/Lec_6_Async/facts/asyncSerial.js
@@ -0,0 +1,23 @@
+// multiple files
+// async code
+// F1kaData >> F2kaData >> F3kaData
+
+const fs = require("fs");
+
+console.log("Start");
+
+fs.readFile("./f1.txt", function (error, data) {
+  console.log(data + "");
+  fs.readFile("./f2.txt", function (error, data) {
+    console.log(data + "");
+    fs.readFile("./f3.txt", function (error, data) {
+      // console.log(err);
+      console.log(data + "");
+    })
+  })
+})
+
+
+
+
+console.log("End");

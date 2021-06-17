05014f6bef365a441594738c9456b84e4a85313c
SUMIT <sumitdogra3236@gmail.com>
2021-06-17 08:43:13 +0530

    CUCHD Scrapping yet to complete

--- Module_1/cuchd/homepage.js
+++ Module_1/cuchd/homepage.js
@@ -1,3 +1,17 @@
 let fs =  require("fs");
 let cheerio = require("cheerio");
 let request = require("request");
+let link = "https://www.cuchd.in/";
+request(link, function(err, res, data){
+    process(data);
+    // console.log(data);
+})
+
+function process(data){
+    let myDocument = cheerio.load(data);
+    let ul = myDocument(
+      ".container-fluid .navbar-right.megamenu.row .navbar-right.megamenu.row .whatsapp.d-flex li"
+    );
+    console.log(ul.length);
+
+}

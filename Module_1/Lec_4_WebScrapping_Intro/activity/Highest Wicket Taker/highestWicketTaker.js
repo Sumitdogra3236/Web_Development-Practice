d64c5d15441f50d852763ce5ab4302a949cb2e52
SUMIT <sumitdogra3236@gmail.com>
2021-06-28 12:21:26 +0530

    Highest Wicket Taker

--- Module_1/Lec_4_WebScrapping_Intro/activity/Highest Wicket Taker/highestWicketTaker.js
+++ Module_1/Lec_4_WebScrapping_Intro/activity/Highest Wicket Taker/highestWicketTaker.js
@@ -1,7 +1,7 @@
 let matchLink =
   "https://www.espncricinfo.com/series/ipl-2020-21-1210595/delhi-capitals-vs-mumbai-indians-final-1237181/full-scorecard";
 const request = require("request");
-const fs = require("fs");
+const fs = require("fs"); 
 const cheerio = require("cheerio");
 
 request(matchLink, cb);

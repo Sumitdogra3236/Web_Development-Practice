867713ca81aa89a74f2da20cb433c675840e88b6
SUMIT <sumitdogra3236@gmail.com>
2021-06-28 12:19:02 +0530

    W Cat

--- Module_1/Lec_3_wCat_Commands/facts/wCat.js
+++ Module_1/Lec_3_wCat_Commands/facts/wCat.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 // cat f1.txt => f1 ka content
 // cat f1.txt f2.txt => f1 ka content + f2 ka content
 
-let f1KaData = fs.readFileSync("./f1.txt", "utf8");
+let f1KaData = fs.readFileSync("./f1.txt", "utf8"); 
 let f2KaData = fs.readFileSync("./f2.txt", "utf8");
 
 // let bothOutput = f1KaData + "\n" +f2KaData;
